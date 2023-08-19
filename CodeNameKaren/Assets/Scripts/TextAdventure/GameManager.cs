ec8cbaf243011417dbbb451ca2a16858b7d010b5
AReeme <areeme@student.neumont.edu>
2023-08-19 16:32:25 -0600

    More Story

--- CodeNameKaren/Assets/Scripts/TextAdventure/GameManager.cs
+++ CodeNameKaren/Assets/Scripts/TextAdventure/GameManager.cs
@@ -30,13 +30,17 @@ public class GameManager : MonoBehaviour
 
     StoryBlock currentBlock;
 
-    static StoryBlock block9 = new StoryBlock("Karen tried calling the cops on the girl, but the cops thought she was crazy and hung up. Karen was livid.", "Fight the child", "Fight the child.");
-    static StoryBlock block8 = new StoryBlock("Karen demanded to see a permit from the girl. The girl started to cry, and didn't know what Karen meant.", "Fight the child.", "Fight the child.");
+
+    static StoryBlock block12 = new StoryBlock("Karen was satisfied with what she did and decided to go home. On her way home, she was tailgated by the child's mother and driven off a cliff. Game Over. Press a button to restart.");
+    static StoryBlock block11 = new StoryBlock("Karen continued to the grocery store. She noticed that there was a Wendy's to her left.", "Continue to the grocery store.", "Go To Wendy's.");
+    static StoryBlock block10 = new StoryBlock("Karen punted the child. She wondered what to do next.", "Continue to the grocery store.", "Go Home.", block11, block12);
+    static StoryBlock block9 = new StoryBlock("Karen tried calling the cops on the girl, but the cops thought she was crazy and hung up. Karen was livid.", "Fight the child", "Fight the child.", block10, block10);
+    static StoryBlock block8 = new StoryBlock("Karen demanded to see a permit from the girl. The girl started to cry, and didn't know what Karen meant.", "Fight the child.", "Fight the child.", block10, block10);
     static StoryBlock block7 = new StoryBlock("Karen tried to ignore the girl, but she couldn't control her urges to confront her. She smiles and asks if Karen wanted to buy any cookies.", "Ask if she has a permit.", "Threaten to call the cops.", block8, block9);
     static StoryBlock block6 = new StoryBlock("Karen chooses to confront the girl. She smiles and asks if Karen wanted to buy any cookies.", "Ask if she has a permit.", "Threaten to call the cops.", block8, block9);
     static StoryBlock block5 = new StoryBlock("Karen drives to grocery store and notices a young girl scout selling cookies.", "Confront the girl.", "Ignore the girl.", block6, block7);
     static StoryBlock block4 = new StoryBlock("Karen walks to grocery store and notices a young girl scout selling cookies.", "Confront the girl.", "Ignore the girl.", block6, block7);
-    static StoryBlock block3 = new StoryBlock("Karen curses out God and gets struck by lightning. Game Over.");
+    static StoryBlock block3 = new StoryBlock("Karen curses out God and gets struck by lightning. Game Over. Press a button to restart.");
     static StoryBlock block2 = new StoryBlock("Karen decides to get dressed and goes to the grocery store.", "Walk to Grocery Store", "Use Car", block4, block5);
     static StoryBlock block1 = new StoryBlock("Karen wakes up in a cold sweat.", "Get Dressed.", "Curse out God.", block2, block3);
 
@@ -48,12 +52,28 @@ public class GameManager : MonoBehaviour
 
     public void Button1Clicked()
     {
-        DisplayBlock(currentBlock.option1Block);
+        if (currentBlock.option1Block != null)
+        {
+            DisplayBlock(currentBlock.option1Block);
+        }
+        else
+        {
+            // Reset the game
+            DisplayBlock(block1);
+        }
     }
 
     public void Button2Clicked()
     {
-        DisplayBlock(currentBlock.option2Block);
+        if (currentBlock.option2Block != null)
+        {
+            DisplayBlock(currentBlock.option2Block);
+        }
+        else
+        {
+            // Reset the game
+            DisplayBlock(block1);
+        }
     }
 
     void DisplayBlock(StoryBlock block) 
